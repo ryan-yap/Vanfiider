7f1f6e8ef8e3dee13dd4e949d0605891c364751e
KangShiang <ykshiang@gmail.com>
2016-03-19 16:10:26 -0700

    added pagemenu

--- Vanfiider/ViewController.swift
+++ Vanfiider/ViewController.swift
@@ -6,12 +6,25 @@
 //  Copyright © 2016 Fiidup.Vanfiider. All rights reserved.
 //
 
+import PagingMenuController
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let viewController1 = self.storyboard?.instantiateViewControllerWithIdentifier("ExploreViewController") as! ExploreViewController
+        viewController1.title = "Explore"
+        let viewController2 = self.storyboard?.instantiateViewControllerWithIdentifier("PantryViewController") as! PantryViewController
+        viewController2.title = "Pantry"
+        
+        let viewControllers = [viewController1, viewController2]
+        
+        var options = PagingMenuOptions()
+        options.menuHeight = 0;
+        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
+        pagingMenuController.setup(viewControllers: viewControllers, options: options)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
