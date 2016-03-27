b50e506b3d6d63566e316dba88fd0882536cac92
KangShiang <ykshiang@gmail.com>
2016-03-27 02:33:46 -0700

    re-added git ignore

--- Pods/Target Support Files/Pods-Vanfiider/Pods-Vanfiider-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Vanfiider : NSObject
-@end
-@implementation PodsDummy_Pods_Vanfiider
-@end
--- Pods/Target Support Files/Pods-VanfiiderTests/Pods-VanfiiderTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VanfiiderTests : NSObject
-@end
-@implementation PodsDummy_Pods_VanfiiderTests
-@end
