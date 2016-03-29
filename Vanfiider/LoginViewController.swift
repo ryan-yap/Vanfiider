960e8d2413e19a560fd2527ee596d8a2de78bad9
chunkiet66 <kiet5629@hotmail.com>
2016-03-28 21:23:23 -0700

    added login page with segue(Note: need to change it if needed)

---
+++ Vanfiider/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Vanfiider
+//
+//  Created by Kang Shiang on 2016-03-27.
+//  Copyright © 2016 Fiidup.Vanfiider. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
